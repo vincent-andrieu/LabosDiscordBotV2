62b66389119444fa45bcea8d255d6581799bc728
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-02-04 18:53:23 +0100

    Edit set model id as optional

--- global/interfaces/database.interface.ts
+++ global/interfaces/database.interface.ts
@@ -4,11 +4,11 @@ export type ObjectId = mongoose.Schema.Types.ObjectId;
 export const ObjectId = mongoose.Schema.Types.ObjectId;
 
 export interface IModel {
-    _id: ObjectId;
+    _id?: ObjectId;
 }
 
 export class CModel implements IModel {
-    _id: ObjectId;
+    _id?: ObjectId;
 
     constructor(model: IModel) {
         this._id = model._id;
