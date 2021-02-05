cae1c4090f0b841b7c892ff35301eef409435a7f
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-02-05 18:26:37 +0100

    Fix help type

--- server/src/commands/help/help.ts
+++ server/src/commands/help/help.ts
@@ -1,10 +1,14 @@
 import { Message } from "discord.js";
 
 import { CServer } from "@global/interfaces/server.interface";
+import { LaboratorySchema } from "@schemas/laboratories.schema";
+import { ProductionSchema } from "@schemas/productions.schema";
+import { StockSchema } from "@schemas/stocks.schema";
+import { ServerSchema } from "@schemas/servers.schema";
 import { CCommand, CommandsList } from "@commands/commands";
 import DiscordBot, { EEmbedMsgColors } from "init/bot";
 
-export function help(server: CServer, command?: CCommand<any>): Promise<Message> | undefined {
+export function help(server: CServer, command?: CCommand<LaboratorySchema | ProductionSchema | StockSchema | ServerSchema>): Promise<Message> | undefined {
     if (command) {
         return command.sendHelp(server);
     }
