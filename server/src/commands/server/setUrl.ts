63edc3504bd481b81d15a903484e825f8bc2ba74
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-02-05 15:55:50 +0100

    Add setUrl cmd

---
+++ server/src/commands/server/setUrl.ts
@@ -0,0 +1,31 @@
+import { CServer } from "@global/interfaces/server.interface";
+import { ServerSchema } from "@schemas/servers.schema";
+import { CCommand, ECommand } from "@commands/commands";
+
+export default class SetUrl extends CCommand<ServerSchema> {
+
+    constructor(helpDesc = "", helpParams = "") {
+        super(new ServerSchema(), ECommand.SERVER_SET_URL, helpDesc, helpParams);
+    }
+
+    private getParamsTemplate(params: Array<string>): string | undefined {
+        if (params.length == 0) {
+            return undefined;
+        }
+        return params[0];
+    }
+
+    public doAction(server: CServer, params: Array<string>): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            const url: string | undefined = this.getParamsTemplate(params);
+
+            if (!url) {
+                return reject("Paramètres de la commande invalide");
+            }
+            this._schema.setUrl(server, url)
+                .then((result) => resolve(result))
+                .catch((err) => reject(err));
+        });
+    }
+
+}
