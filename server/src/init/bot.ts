f6cc1dcfd96809e4f853c58bc812893d921d6746
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-02-06 17:14:41 +0100

    Remove log

--- server/src/init/bot.ts
+++ server/src/init/bot.ts
@@ -21,7 +21,6 @@ export default class DiscordBot {
             client.on('error', console.error);
             client.on('warn', console.warn);
             client.login(this.getBotToken());
-            console.log(this.getBotToken());
 
             client.on('ready', () => {
                 console.info("Bot ready !");
