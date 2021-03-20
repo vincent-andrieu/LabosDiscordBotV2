bf2d83de36226eb5d84664cdfd7de878aef1e1d1
Vincent Andrieu <vincent.andrieu@epitech.eu>
2021-03-20 19:03:34 +0100

    Add deploy client as production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+import { env } from 'process';
+
 export const environment = {
-  production: true
+    production: true,
+    cookiesName: {
+        sidenavStatus: 'sidenavStatus',
+        discordUserId: 'discordUserId'
+    },
+    sockets: {
+        url: env['SOCKETS_URL'] ||  'http://localhost:3000'
+    },
+    server: {
+        url: env['SERVER_URL'] || 'http://localhost:8080'
+    }
 };
